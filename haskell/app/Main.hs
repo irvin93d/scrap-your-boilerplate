c6b4479427df0c94c9919dd3b9b864e77ccb3dea
Duane Irvin <irvin93d@gmail.com>
2021-11-11 12:54:55 +0100

    queries

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -9,25 +9,54 @@ main = do
   print genCom
   print $ increase 0.1 genCom
   print $ increase' 0.1 genCom
+  print $ salaryBill genCom
+  print $ salaryBill' genCom
 
-genCom :: Company
-genCom =
-  Company
-    [ Department "Research" ralf [EmployeeUnit joost, EmployeeUnit marlow],
-      Department "Strategy" blair []
-    ]
+salaryBill :: Company -> Float
+salaryBill (Company ds) = sum $ map salaryBillDepartment ds
 
-ralf, joost, marlow, blair :: Employee
-ralf = Employee (Person "Ralf" "Amsterdam") (Salary 8000)
-joost = Employee (Person "Joost" "Amsterdam") (Salary 1000)
-marlow = Employee (Person "Marlow" "Cambridge") (Salary 2000)
-blair = Employee (Person "Blair" "London") (Salary 100000)
+salaryBill' :: Company -> Float
+salaryBill' = everything (+) (0 `mkQ` salaryBillSalary)
+
+salaryBillDepartment :: Department -> Float
+salaryBillDepartment (Department _ manager subunits) =
+  salaryBillEmployee manager + sum (map salaryBillSubUnit subunits)
+
+salaryBillSubUnit :: SubUnit -> Float
+salaryBillSubUnit (EmployeeUnit employee) = salaryBillEmployee employee
+salaryBillSubUnit (DepartmentUnit department) = salaryBillDepartment department
+
+salaryBillEmployee :: Employee -> Float
+salaryBillEmployee (Employee _ salary) = salaryBillSalary salary
+
+salaryBillSalary :: Salary -> Float
+salaryBillSalary (Salary value) = value
 
 increase :: Float -> Company -> Company
 increase k (Company ds) = Company (map (incDepartment k) ds)
 
 increase' :: Float -> Company -> Company
 increase' k = everywhere (mkT (increaseSalary k))
+
+increaseSalary :: Float -> Salary -> Salary
+increaseSalary k (Salary s) = Salary (s * (1 + k))
+
+genCom :: Company
+genCom =
+  Company
+    [ Department "Research" ralf [EmployeeUnit joost, EmployeeUnit marlow, DepartmentUnit boilerscrap],
+      Department "Strategy" blair []
+    ]
+
+ralf, joost, marlow, blair, bill :: Employee
+ralf = Employee (Person "Ralf" "Amsterdam") (Salary 8000)
+joost = Employee (Person "Joost" "Amsterdam") (Salary 1000)
+marlow = Employee (Person "Marlow" "Cambridge") (Salary 2000)
+blair = Employee (Person "Blair" "London") (Salary 100000)
+bill = Employee (Person "Bill Gates" "Medina") (Salary 1000000)
+
+boilerscrap :: Department
+boilerscrap = Department "Boilerplate Scraping" bill []
 
 incDepartment :: Float -> Department -> Department
 incDepartment k (Department nm mgr us) = Department nm (increaseEmployee k mgr) (map (increaseSubUnit k) us)
@@ -38,6 +67,3 @@ increaseSubUnit k (DepartmentUnit d) = DepartmentUnit (incDepartment k d)
 
 increaseEmployee :: Float -> Employee -> Employee
 increaseEmployee k (Employee p s) = Employee p (increaseSalary k s)
-
-increaseSalary :: Float -> Salary -> Salary
-increaseSalary k (Salary s) = Salary (s * (1 + k))
--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -7,6 +7,7 @@ import Data.Typeable
 
 class Typeable a => Term a where
   gmapT :: (forall b. Term b => b -> b) -> a -> a
+  gmapQ :: (forall b. Term b => b -> r) -> a -> [r]
 
 everywhere :: Term a => (forall b. Term b => b -> b) -> a -> a
 everywhere f x = f (gmapT (everywhere f) x)
@@ -16,3 +17,9 @@ everywhere' f x = gmapT (everywhere' f) (f x)
 
 mkT :: (Typeable a, Typeable b) => (b -> b) -> a -> a
 mkT f = fromMaybe id (cast f)
+
+everything :: Term a => (r -> r -> r) -> (forall a. Term a => a -> r) -> a -> r
+everything k f x = foldl k (f x) (gmapQ (everything k f) x)
+
+mkQ :: (Typeable a, Typeable b) => r -> (b -> r) -> a -> r
+(r `mkQ` q) a = maybe r q (cast a)
