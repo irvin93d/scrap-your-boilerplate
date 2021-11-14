2346d9d60ea4bf2ad83d18d4b143bb3b6b738f99
Duane Irvin <irvin93d@gmail.com>
2021-11-14 16:43:18 +0100

    remove typescript

--- typescript/src/boilerplate.ts
+++
@@ -1,7 +0,0 @@
-import { Salary } from "./model";
-
-export const salaryMatcher = (data: unknown): data is Salary =>
-  typeof data === "object" &&
-  !!data &&
-  (data as any).type === "salary" &&
-  typeof (data as any).value === "number";
